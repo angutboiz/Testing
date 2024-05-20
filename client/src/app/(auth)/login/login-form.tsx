1ffe5ef80af19693ba1e4ed74555334f7fe9ae11
minhnghia2k3 <BrozennVN@gmail.com>
2024-05-20 22:13:38 +0700

    fix: Button register not submit form

--- client/src/app/(auth)/login/login-form.tsx
+++ client/src/app/(auth)/login/login-form.tsx
@@ -5,7 +5,14 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 
 import { Button } from "@/components/ui/button";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { LoginBodyType, LoginBody } from "@/schemaValidations/auth.schema";
 import { useToast } from "@/components/ui/use-toast";
@@ -13,72 +20,81 @@ import { useRouter } from "next/navigation";
 import authApiRequest from "@/lib/auth";
 
 export default function LoginForm() {
-    const { toast } = useToast();
-    const router = useRouter();
+  const { toast } = useToast();
+  const router = useRouter();
 
-    const form = useForm<LoginBodyType>({
-        resolver: zodResolver(LoginBody),
-        defaultValues: {
-            email: "",
-            password: "",
-        },
-    });
+  const form = useForm<LoginBodyType>({
+    resolver: zodResolver(LoginBody),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
-    async function onSubmit(values: LoginBodyType) {
-        try {
-            const response = await authApiRequest.login(values);
-
-            if (response.status === 400) {
-                toast({
-                    variant: "destructive",
-                    title: "Tên tài khoản hoặc mật khẩu sai?",
-                });
-            } else {
-                toast({
-                    variant: "success",
-                    title: "Đăng nhập thành công!",
-                });
-                router.push("/");
-            }
-        } catch (error) {
-            toast({
-                variant: "destructive",
-                title: "Đã xảy ra lỗi trong quá trình kết nối tới server.",
-            });
-        }
+  async function onSubmit(values: LoginBodyType) {
+    console.log("submit");
+    try {
+      const response = await authApiRequest.login(values);
+      console.log("response: ", response);
+      if (response.status === 400) {
+        toast({
+          variant: "destructive",
+          title: "Tên tài khoản hoặc mật khẩu sai?",
+        });
+      } else {
+        toast({
+          variant: "success",
+          title: "Đăng nhập thành công!",
+        });
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+      toast({
+        variant: "destructive",
+        title: "Đã xảy ra lỗi trong quá trình kết nối tới server.",
+      });
     }
+  }
 
-    return (
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 " noValidate>
-                <FormField
-                    control={form.control}
-                    name="email"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Email</FormLabel>
-                            <FormControl>
-                                <Input placeholder="Nhập email của bạn (VD: example@gmail.com)" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />{" "}
-                <FormField
-                    control={form.control}
-                    name="password"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Password</FormLabel>
-                            <FormControl>
-                                <Input placeholder="Nhập password" {...field} type="password" />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />{" "}
-                <Button type="submit">Đăng nhập</Button>
-            </form>
-        </Form>
-    );
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 "
+        noValidate
+      >
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="Nhập email của bạn (VD: example@gmail.com)"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />{" "}
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder="Nhập password" {...field} type="password" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />{" "}
+        <Button type="submit">Đăng nhập</Button>
+      </form>
+    </Form>
+  );
 }
--- client/src/app/(auth)/register/register-form.tsx
+++ client/src/app/(auth)/register/register-form.tsx
@@ -4,127 +4,152 @@ import React from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { RegisterBody, RegisterBodyType, RegisterThreeField } from "@/schemaValidations/auth.schema";
+import {
+  RegisterBody,
+  RegisterBodyType,
+  RegisterThreeField,
+} from "@/schemaValidations/auth.schema";
 import { useToast } from "@/components/ui/use-toast";
 import { useRouter } from "next/navigation";
 import authApiRequest from "@/lib/auth";
 
 export default function RegisterForm() {
-    const { toast } = useToast();
-    const router = useRouter();
+  const { toast } = useToast();
+  const router = useRouter();
 
-    const form = useForm<RegisterBodyType>({
-        resolver: zodResolver(RegisterBody),
-        defaultValues: {
-            email: "",
-            username: "",
-            password: "",
-            confirmPassword: "",
-        },
-    });
+  const form = useForm<RegisterBodyType>({
+    resolver: zodResolver(RegisterBody),
+    defaultValues: {
+      email: "",
+      username: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
 
-    async function onSubmit(values: RegisterThreeField) {
-        console.log("hello");
-        try {
-            const response = await authApiRequest.register(values);
+  async function onSubmit(values: RegisterThreeField) {
+    try {
+      const response = await authApiRequest.register(values);
 
-            if (response.status === 409) {
-                toast({
-                    variant: "destructive",
-                    title: "Tên người dùng hoặc mật khẩu đã đăng ký",
-                });
-            } else if (response.status === 400) {
-                toast({
-                    variant: "destructive",
-                    title: "Tên tài khoản không được viết hoa, không có khoảng trống, không kí tự đặc biệt",
-                });
-            } else {
-                toast({
-                    variant: "success",
-                    title: "Đăng ký thành công!",
-                });
-                router.push("/login");
-            }
-        } catch (error) {
-            toast({
-                variant: "destructive",
-                title: "Đã xảy ra lỗi trong quá trình kết nối tới server.",
-            });
-        }
+      if (response.status === 409) {
+        toast({
+          variant: "destructive",
+          title: "Tên người dùng hoặc mật khẩu đã đăng ký",
+        });
+      } else if (response.status === 400) {
+        toast({
+          variant: "destructive",
+          title:
+            "Tên tài khoản không được viết hoa, không có khoảng trống, không kí tự đặc biệt",
+        });
+      } else {
+        toast({
+          variant: "success",
+          title: "Đăng ký thành công!",
+        });
+        router.push("/login");
+      }
+    } catch (error) {
+      toast({
+        variant: "destructive",
+        title: "Đã xảy ra lỗi trong quá trình kết nối tới server.",
+      });
     }
+  }
 
-    return (
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8" noValidate>
-                <div className="flex justify-between gap-5">
-                    <div className="flex-1">
-                        <FormField
-                            control={form.control}
-                            name="username"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Tài khoản</FormLabel>
-                                    <FormControl>
-                                        <Input placeholder="Nhập tài khoản (VD: trongandev)" {...field} />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />{" "}
-                    </div>
-                    <div className="flex-1">
-                        <FormField
-                            control={form.control}
-                            name="email"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Email</FormLabel>
-                                    <FormControl>
-                                        <Input placeholder="Nhập email của bạn (VD: example@gmail.com)" {...field} />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />{" "}
-                    </div>
-                </div>
-                <div className="flex justify-between gap-5">
-                    <div className="flex-1">
-                        <FormField
-                            control={form.control}
-                            name="password"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Mật khẩu</FormLabel>
-                                    <FormControl>
-                                        <Input placeholder="Nhập mật khẩu" {...field} type="password" />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />{" "}
-                    </div>
-                    <div className="flex-1">
-                        <FormField
-                            control={form.control}
-                            name="confirmPassword"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Nhập lại mật khẩu</FormLabel>
-                                    <FormControl>
-                                        <Input placeholder="Nhập lại mật khẩu" {...field} type="password" />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                    </div>
-                </div>
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <div className="flex justify-between gap-5">
+          <div className="flex-1">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Tài khoản</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Nhập tài khoản (VD: trongandev)"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />{" "}
+          </div>
+          <div className="flex-1">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Nhập email của bạn (VD: example@gmail.com)"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />{" "}
+          </div>
+        </div>
+        <div className="flex justify-between gap-5">
+          <div className="flex-1">
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Mật khẩu</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Nhập mật khẩu"
+                      {...field}
+                      type="password"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />{" "}
+          </div>
+          <div className="flex-1">
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nhập lại mật khẩu</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Nhập lại mật khẩu"
+                      {...field}
+                      type="password"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+        </div>
 
-                <Button type="submit">Đăng kí</Button>
-            </form>
-        </Form>
-    );
+        <Button type="submit">Đăng kí</Button>
+      </form>
+    </Form>
+  );
 }
